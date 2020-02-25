00b8875ad7c77956ce46fbf8be44d3be4408e4cc
Nathan <cpjsmith1@gmail.com>
2020-02-25 02:58:08 -0500

    final commit, I will become a React pro!

--- nathanok.github.io/src/App.js
+++ nathanok.github.io/src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/nathanok"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me Out
         </a>
       </header>
     </div>
